6cbcce26955eab1e332d4beac0f3ae6aa9c977a4
LocNguyen <phoenician93@gmail.com>
2018-11-22 11:32:58 +0700

    custom user

--- users/admin.py
+++ users/admin.py
@@ -3,13 +3,38 @@ from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from django.contrib.auth.models import Group
 
 # Register your models here.
 
+# class CustomUserAdmin(UserAdmin):
+#     add_form = CustomUserCreationForm
+#     form = CustomUserChangeForm
+#     model = CustomUser
+#     list_display = ['email', 'username', ]
+
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username', ]
+    add_form = CustomUserCreationForm
+    list_display = ('username', 'email', 'date_of_birth', 'is_superuser')
+    list_filter = ('is_superuser',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Permissons', {'fields': ('is_superuser',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('usersname', 'email', 'date_of_birth', 'password1', 'password2')
+            }
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
